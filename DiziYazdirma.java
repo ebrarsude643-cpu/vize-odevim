cb1b55d616c6ddc5640d1be7fe2764e43370fed5
ebrarsude643-cpu <ebrarsude643@gmail.com>
2025-11-27 19:41:55 +0300

    Create DiziYazdirma.java

---
+++ DiziYazdirma.java
@@ -0,0 +1,11 @@
+public class DiziYazdirma {
+    public static void main(String[] args) {
+        int[] sayilar = {10, 20, 30, 40, 50};
+
+        System.out.println("Dizideki elemanlar:");
+
+        for (int i = 0; i < sayilar.length; i++) {
+            System.out.println(sayilar[i]);
+        }
+    }
+}
